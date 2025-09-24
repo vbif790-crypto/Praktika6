2a9336c1934cbd2e63be5dffa5c6a5b35c737bc6
315 <315@315-16>
2025-09-24 09:17:04 +0300

    выведены все компьютеры

--- Program.cs
+++ Program.cs
@@ -26,5 +26,21 @@ class Program
             return $"ID: {ID}, Марка: {Brand}, ОЗУ: {RAM} Гб, ДП: {DiskSpace} Гб, Скорость: {Speed} ГГц";
         }
     }
+    static void Main()
+    {
+        List<Computer> computers = new List<Computer>();
 
-   
+        computers.Add(new Computer(1, "Apple", 12, 200, 2));
+        computers.Add(new Computer(2, "Asus", 6, 1500, 2.3));
+        computers.Add(new Computer(3, "Lenovo", 8, 600, 2.5));
+        computers.Add(new Computer(4, "Assus", 6, 1000, 4));
+        computers.Add(new Computer(5, "Apple", 4, 1400, 2.8));
+
+        Console.WriteLine("Все компьютеры в коллекции:");
+        foreach (var computer in computers)
+        {
+            Console.WriteLine(computer);
+        }
+        Console.WriteLine();
+
+        
